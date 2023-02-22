083805ee49e57bd41227f82f207e663a04426ef1
Simon <sillen.00@hotmail.com>
2023-02-22 15:42:09 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1>HEKKLKOIOOOOOoo</h1>
+    </div>
+  )
+}
+
+export default App
