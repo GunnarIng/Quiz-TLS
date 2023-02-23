36f070cf61d8b0ea707d7eaf7d9075324a49860b
Linus Hernvall <linus.hernvall@gmail.com>
2023-02-23 15:34:23 +0100

    Installed JSS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
-import { createUseStyles } from 'react-jss';
-import { Header } from './Components/Header';
+import { createUseStyles } from "react-jss";
+import { Header } from "./Components/Header";
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <div className="App">
       <Header />
-      <h1>helli <span>sluta</span></h1>
+      <h1>
+        helli <span>sluta</span>
+      </h1>
       <h1>kopeter</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
-
-const useStyles = createUseStyles({
-
-});
-
+const useStyles = createUseStyles({});
 
 // const useStyles = createUseStyles({
 //   myH1: {
 //     color: 'green',
 //     margin: "5rem 3rem 0 0",
 //     border: "1px solid red",
-    
+
 //     '& span': {
 //       color: "pink",
 //     },
 //   },
-  
+
 //   myH2: {
 //     color: "orange",
 //     boxShadow:"0px 10px 8px black",
@@ -41,5 +39,5 @@ const useStyles = createUseStyles({
 //       color: "blue",
 //     }
 //   },
-  
+
 // });
