7739c4cfb08e2fdebebc13cc662c6188f9d349cb
Simon <sillen.00@hotmail.com>
2023-02-28 11:24:08 +0100

    Added a Error Boundary file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider,
+  RouterProvider
 } from "react-router-dom";
 import App from "./App";
+import ErrorBoundary from "./ErrorBoundary";
 import "./index.css";
 import { HomePage } from "./pages/HomePage";
 import { QuizPage } from "./pages/QuizPage";
@@ -14,11 +14,15 @@ import { Result } from "./pages/Result";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+    <ErrorBoundary>
+
     <Route path="/" element={<App />}>
       <Route index element={<HomePage />} />
       <Route path=":category" element={<QuizPage />} />
       <Route path="result" element={<Result />} />
     </Route>
+
+    </ErrorBoundary>
   )
 );
 
