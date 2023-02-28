9131afb3184ef250dd8cbfb4d36a5fe846b7caad
Simon <sillen.00@hotmail.com>
2023-02-28 13:25:12 +0100

    Flyttade ut homeButton till en egen komponent.

---
+++ src/Components/HomeButton.tsx
@@ -0,0 +1,37 @@
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { createUseStyles } from "react-jss";
+
+export function HomeButton() {
+    const classes = useStyles()
+  return (
+    <div className={classes.homeBtn}>
+      {<FontAwesomeIcon icon={faHouse} />}
+      HOME
+    </div>
+  );
+}
+
+const useStyles = createUseStyles({
+  homeBtn: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "right",
+    gap: "0.6rem",
+    position: "absolute",
+    top: "50%",
+    fontSize: "1.5rem",
+    left: "-1.8rem",
+    padding: "0.6rem 1.25rem 0.6rem 2.2rem",
+    borderRadius: "10000rem",
+    transform: "translateY(-50%)",
+    backgroundColor: "#3e3a44",
+    textDecoration: "none",
+    color: "white",
+    "&:hover": {
+      color: "#3e3a44",
+      background: "white",
+      transition: "all 0.3s ease-in-out",
+    },
+  },
+});
--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,8 +1,7 @@
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { createUseStyles } from "react-jss";
 import { NavLink, useParams } from "react-router-dom";
 import { AnswerButton } from "../Components/AnswerButton";
+import { HomeButton } from "../Components/HomeButton";
 import { useCategoryColor } from "../hooks/useCategoryColor";
 
 export function QuizPage() {
@@ -46,10 +45,7 @@ export function QuizPage() {
         {categoryName}
 
         <NavLink to="/">
-          <div className={classes.homeBtn}>
-            {<FontAwesomeIcon icon={faHouse} />}
-            HOME
-          </div>
+          <HomeButton />
         </NavLink>
       </h4>
       <div
@@ -59,12 +55,18 @@ export function QuizPage() {
         <div>This is where the api inputs will show</div>
       </div>
       <div className={classes.answerContainer}>
-        <AnswerButton bgColor={categoryColor.backgroundColor}> answer 1</AnswerButton>
-        <AnswerButton bgColor={categoryColor.backgroundColor}> answer 2</AnswerButton>
-        <AnswerButton bgColor={categoryColor.backgroundColor}> answer 3</AnswerButton>
-        <AnswerButton bgColor={categoryColor.backgroundColor}> answer 4</AnswerButton>
-        
-        
+        <AnswerButton bgColor={categoryColor.backgroundColor}>
+          answer 1
+        </AnswerButton>
+        <AnswerButton bgColor={categoryColor.backgroundColor}>
+          answer 2
+        </AnswerButton>
+        <AnswerButton bgColor={categoryColor.backgroundColor}>
+          answer 3
+        </AnswerButton>
+        <AnswerButton bgColor={categoryColor.backgroundColor}>
+          answer 4
+        </AnswerButton>
       </div>
     </div>
   );
@@ -93,26 +95,5 @@ const useStyles = createUseStyles({
     padding: "2rem 0 0 0",
     gap: "1rem",
     margin: "0 auto",
-  },
-  homeBtn: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "right",
-    gap: "0.6rem",
-    position: "absolute",
-    top: "50%",
-    fontSize: "1.5rem",
-    left: "-1.8rem",
-    padding: "0.6rem 1.25rem 0.6rem 2.2rem",
-    borderRadius: "10000rem",
-    transform: "translateY(-50%)",
-    backgroundColor: "#3e3a44",
-    textDecoration: "none",
-    color: "white",
-    "&:hover": {
-      color: "#3e3a44",
-      background: "white",
-      transition: "all 0.3s ease-in-out",
-    },
   },
 });
