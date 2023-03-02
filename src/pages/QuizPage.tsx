47d79c1ab6e5c630b0d3f928eb20e58acf42d7c7
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-03-02 14:59:36 +0100

    all good here

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,11 +1,9 @@
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { createUseStyles } from "react-jss";
 import { NavLink, useParams } from "react-router-dom";
 import { AnswerButton } from "../Components/AnswerButton";
-import { Result } from "../Components/Result";
 import { HomeButton } from "../Components/HomeButton";
+import { Result } from "../Components/Result";
 import ErrorBoundary from "../ErrorBoundary";
 import { useCategoryColor } from "../hooks/useCategoryColor";
 import { useCategoryName } from "../hooks/useCategoryName";
@@ -23,57 +21,57 @@ export function QuizPage() {
   // Härlett State
   const currentQuestion = quiz[currentQuestionIndex];
 
-
-
   const handleAnswerClick = (buttonText: string) => {
     const correctAnswer = currentQuestion.correct_answer;
 
     if (correctAnswer.includes(buttonText)) {
       console.log("RÄTT");
       setCorrectAnswers((prevCorrectAnswers) => prevCorrectAnswers + 1);
-    } 
+    }
 
     setCurrentQuestionIndex((prevQuestionIndex) => prevQuestionIndex + 1);
   };
 
   return (
-  <ErrorBoundary message="Something went wrong. Try reload the page.">
-    <div>
-      <h4 className={classes.subjectTitle}>
-        {categoryName} {currentQuestionIndex}/10
-        <NavLink to="/">
+    <ErrorBoundary message="Something went wrong. Try reload the page.">
+      <div className={classes.rootContainer}>
+        <h4 className={classes.subjectTitle}>
+          {categoryName} {currentQuestionIndex}/10
+          <NavLink to="/">
             <HomeButton />
           </NavLink>
-      </h4>
+        </h4>
 
-      {quiz.length > 0 && currentQuestionIndex < 10 && (
-        <div
-          className={classes.questionBox}
-          style={{ backgroundColor: categoryColor.backgroundColor }}
-        >
-          {currentQuestion.question}
-        </div>
-      )}
+        {quiz.length > 0 && currentQuestionIndex < 10 && (
+          <div
+            className={classes.questionBox}
+            style={{ backgroundColor: categoryColor.backgroundColor }}
+          >
+            {currentQuestion.question}
+          </div>
+        )}
 
-    <ErrorBoundary message="Something went wrong. Try reload the page.">
-      {currentQuestion?.answers.map((answer) => (
-        <AnswerButton
-          key={answer}
-          onClick={handleAnswerClick}
-          bgColor={categoryColor.backgroundColor}
-        >
-          {answer}
-        </AnswerButton>
-      ))}
-       </ErrorBoundary>
-      
-      {/* Render your "Result" component and pass the number of correct answers as a prop */}
-      {currentQuestionIndex >= 10 && (
-        <Result color={categoryColor.backgroundColor} correctAnswers={correctAnswers} />
-      )}
-    </div>
+        <ErrorBoundary message="Something went wrong. Try reload the page.">
+          {currentQuestion?.answers.map((answer) => (
+            <AnswerButton
+              key={answer}
+              onClick={handleAnswerClick}
+              bgColor={categoryColor.backgroundColor}
+            >
+              {answer}
+            </AnswerButton>
+          ))}
+        </ErrorBoundary>
+
+        {/* Render your "Result" component and pass the number of correct answers as a prop */}
+        {currentQuestionIndex >= 10 && (
+          <Result
+            color={categoryColor.backgroundColor}
+            correctAnswers={correctAnswers}
+          />
+        )}
+      </div>
     </ErrorBoundary>
-
   );
 }
 
@@ -81,7 +79,7 @@ const useStyles = createUseStyles({
   subjectTitle: {
     fontSize: "2rem",
     textAlign: "center",
-    padding: "4rem 0",
+    padding: "4rem 0 2rem",
     position: "relative",
   },
   questionBox: {
@@ -91,13 +89,17 @@ const useStyles = createUseStyles({
     alignItems: "center",
     width: "70%",
     margin: "0 auto",
-    padding: "1rem",
-    height: "18.75rem",
+    // height: "18.75rem",
     color: "white",
+    padding: "3rem 1rem",
+    "@media (min-width: 768px)": {
+      padding: "6rem 6rem",
+    },
   },
-  answerContainer: {
+  rootContainer: {
     display: "grid",
-    padding: "2rem 0 0 0",
+    textAlign: "center",
+    padding: "0.5rem 0 2rem 0",
     gap: "1rem",
     margin: "0 auto",
   },
