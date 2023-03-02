3b0013f589fa13ec7308479886026854b86e402b
Linus Hernvall <linus.hernvall@gmail.com>
2023-03-02 10:37:37 +0100

    Removed unnecessary styling.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,3 +1,4 @@
+import classnames from "classnames";
 import { useState } from "react";
 import { createUseStyles } from "react-jss";
 import { DarkMode } from "./DarkMode";
@@ -13,11 +14,10 @@ export function Header() {
   return (
     <>
       <div
-        className={
-          mode === "light"
-            ? classes.headerContainerLight
-            : classes.headerContainerDark
-        }
+        className={classnames(
+          classes.headerContainer,
+          mode === "light" ? classes.light : classes.dark
+        )}
       >
         <h1>The Quizie-Quiz Game</h1>
         <DarkMode mode={mode} toggleMode={toggleMode} />
@@ -27,10 +27,7 @@ export function Header() {
 }
 
 const useStyles = createUseStyles({
-  headerContainerLight: {
-    color: "black",
-    background: "white",
-    borderBottom: "2px solid #3e3a44",
+  headerContainer: {
     height: "5.3rem",
     display: "flex",
     justifyContent: "center",
@@ -48,26 +45,15 @@ const useStyles = createUseStyles({
       },
     },
   },
-  headerContainerDark: {
+
+  light: {
+    color: "black",
+    background: "white",
+    borderBottom: "2px solid #3e3a44",
+  },
+  dark: {
     color: "white",
     background: "black",
     borderBottom: "2px solid #3e3a44",
-    height: "5.3rem",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-
-    "& h1": {
-      textAlign: "center",
-      fontSize: "1.5rem",
-      padding: "1rem 0",
-
-      "@media (min-width: 768px)": {
-        fontSize: "2rem",
-      },
-      "@media (min-width: 1024px)": {
-        fontSize: "3rem",
-      },
-    },
   },
 });
